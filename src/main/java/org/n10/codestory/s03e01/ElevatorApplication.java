946c4a14df9fb79c81eeeb72a374c779db105960
Laurent <laurent.ruaud@serli.com>
2013-11-06 23:51:10 +0100

    Support des deux versions de reset

--- src/main/java/org/n10/codestory/s03e01/ElevatorApplication.java
+++ src/main/java/org/n10/codestory/s03e01/ElevatorApplication.java
@@ -26,69 +26,75 @@ public class ElevatorApplication extends HttpServlet {
 	 */
 	private static final long serialVersionUID = 1L;
 	private ElevatorEngine elevator = new StateSmartElevator();
-	
+
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String path = request.getPathInfo();
-		if(path == null) {
+		if (path == null) {
 			path = request.getServletPath();
 		}
 		String target = path.substring(path.lastIndexOf("/"));
 		switch (target) {
-        case "/nextCommand":
-            synchronized (elevator) {
-                Command nextCommand = elevator.nextCommand();
-                response.getWriter().println(nextCommand);
-//                baseRequest.getResponse().getWriter().println(nextCommand);
-                System.out.println(String.format("%s %s", target, nextCommand));
-            }
-            break;
-        case "/call":
-            Integer atFloor = Integer.valueOf(request.getParameter("atFloor"));
-            Direction to = Direction.valueOf(request.getParameter("to"));
-            synchronized (elevator) {
-                elevator.call(atFloor, to);
-            }
-            System.out.println(String.format("%s atFloor %d to %s", target, atFloor, to));
-            break;
-        case "/go":
-            Integer floorToGo = Integer.valueOf(request.getParameter("floorToGo"));
-            synchronized (elevator) {
-                elevator.go(floorToGo);
-            }
-//            logger.info(format("%s floorToGo %d", target, floorToGo));
-            break;
-        case "/userHasEntered":
-        case "/userHasExited":
-        	System.out.println(target);
-            break;
-        case "/limit":
-            Integer threshold = Integer.valueOf(request.getParameter("threshold"));
-            synchronized (elevator) {
-                elevator.limit(threshold);
-            }
-            System.out.println(String.format("%s limit to %d", target, threshold));
-            break;
-        case "/reset":
-            Integer lowerFloor = null;//Integer.valueOf(request.getParameter("lowerFloor"));
-            Integer higherFloor = null;//Integer.valueOf(request.getParameter("higherFloor"));
-            String cause = request.getParameter("cause");
-            synchronized (elevator) {
-                elevator.reset(lowerFloor, higherFloor, cause);
-            }
-            System.out.println(String.format("%s cause %s", target, cause));
-            break;
-        default:
-        	System.out.println(target);
-    }
-//    baseRequest.setHandled(true);
+		case "/nextCommand":
+			synchronized (elevator) {
+				Command nextCommand = elevator.nextCommand();
+				response.getWriter().println(nextCommand);
+				// baseRequest.getResponse().getWriter().println(nextCommand);
+				System.out.println(String.format("%s %s", target, nextCommand));
+			}
+			break;
+		case "/call":
+			Integer atFloor = Integer.valueOf(request.getParameter("atFloor"));
+			Direction to = Direction.valueOf(request.getParameter("to"));
+			synchronized (elevator) {
+				elevator.call(atFloor, to);
+			}
+			System.out.println(String.format("%s atFloor %d to %s", target, atFloor, to));
+			break;
+		case "/go":
+			Integer floorToGo = Integer.valueOf(request.getParameter("floorToGo"));
+			synchronized (elevator) {
+				elevator.go(floorToGo);
+			}
+			// logger.info(format("%s floorToGo %d", target, floorToGo));
+			break;
+		case "/userHasEntered":
+		case "/userHasExited":
+			System.out.println(target);
+			break;
+		case "/limit":
+			Integer threshold = Integer.valueOf(request.getParameter("threshold"));
+			synchronized (elevator) {
+				elevator.limit(threshold);
+			}
+			System.out.println(String.format("%s limit to %d", target, threshold));
+			break;
+		case "/reset":
+			Integer lowerFloor = null;
+			if (request.getParameter("lowerFloor") != null) {
+				lowerFloor = Integer.valueOf(request.getParameter("lowerFloor"));
+			}
+			Integer higherFloor = null;
+			if (request.getParameter("higherFloor") != null) {
+				higherFloor = Integer.valueOf(request.getParameter("higherFloor"));
+			}
+			String cause = request.getParameter("cause");
+			synchronized (elevator) {
+				elevator.reset(lowerFloor, higherFloor, cause);
+			}
+			System.out.println(String.format("%s cause %s", target, cause));
+			break;
+		default:
+			System.out.println(target);
+		}
+		// baseRequest.setHandled(true);
 	}
-	
+
 	@Override
 	protected void doPost(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
 		super.doGet(arg0, arg1);
 	}
-	
+
 	@Produces
 	public Set<Service> getServices() {
 		return Collections.emptySet();
