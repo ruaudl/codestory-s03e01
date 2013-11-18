925f3df206e2c39b0eac551945bf4d39760d145a
Laurent <laurent.ruaud@serli.com>
2013-11-18 22:33:49 +0100

    Ajout de scriptspour l'installation sur serveur via SSH

---
+++ server/src/main/script/codestory-s03e01.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+### BEGIN INIT INFO
+# Provides:          codestory-s03e01
+# Short-Description: codestory-s03e01 daemon
+# Description:       CodeStory S03E01
+# Required-Start:    $local_fs $network $remote_fs
+# Required-Stop:     $local_fs $network $remote_fs
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+### END INIT INFO
+
+DAEMON_PATH="/home/org.n10"
+
+DAEMON=java
+DAEMONOPTS="-jar codestory-s03e01.jar"
+
+NAME=codestory-s03e01
+DESC="CodeStory S03E01"
+PIDFILE=/var/run/$NAME.pid
+SCRIPTNAME=/etc/init.d/$NAME
+
+case "$1" in
+    start)
+        printf "%-50s" "Starting $NAME..."
+        cd $DAEMON_PATH
+        PID=`$DAEMON $DAEMONOPTS > /dev/null 2>&1 & echo $!`
+        #echo "Saving PID" $PID " to " $PIDFILE
+        if [ -z $PID ]; then
+            printf "%s\n" "Fail"
+        else
+            echo $PID > $PIDFILE
+            printf "%s\n" "Ok"
+        fi
+    ;;
+    status)
+        printf "%-50s" "Checking $NAME..."
+        if [ -f $PIDFILE ]; then
+            PID=`cat $PIDFILE`
+            if [ -z "`ps axf | grep ${PID} | grep -v grep`" ]; then
+                printf "%s\n" "Process dead but pidfile exists"
+            else
+                echo "Running"
+            fi
+        else
+            printf "%s\n" "Service not running"
+        fi
+    ;;
+    stop)
+        printf "%-50s" "Stopping $NAME"
+            PID=`cat $PIDFILE`
+            cd $DAEMON_PATH
+        if [ -f $PIDFILE ]; then
+            kill -HUP $PID
+            printf "%s\n" "Ok"
+            rm -f $PIDFILE
+        else
+            printf "%s\n" "pidfile not found"
+        fi
+    ;;
+    restart)
+        $0 stop
+        $0 start
+    ;;
+    *)
+        echo "Usage: $0 {status|start|stop|restart}"
+        exit 1
+esac
