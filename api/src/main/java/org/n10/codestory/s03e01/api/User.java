942375960f00ce94ce291c6290229f5e5e6c73c8
Guillaume <guillaume.soldera@gmail.com>
2013-11-27 19:02:41 +0100

    ajout de méthodes pour le calcul de points

--- api/src/main/java/org/n10/codestory/s03e01/api/User.java
+++ api/src/main/java/org/n10/codestory/s03e01/api/User.java
@@ -18,6 +18,7 @@ public class User {
 		this.tickToWait = 0;
 		this.directionToGo = directionToGo;
 		this.initialFloor = initialFloor;
+		this.floorToGo = initialFloor;
 		this.state = State.WAITING;
 	}
 
@@ -90,7 +91,55 @@ public class User {
 		this.floorToGo = floorToGo;
 	}
 
+	public Integer getPoints(int waitTick, int goTick) {
+		return 20 + (2 + Math.abs(initialFloor - floorToGo)) - (waitTick/2 + goTick);
+	}
 	
+	public Integer getRemainingPoints() {
+		return getPoints(tickToWait, tickToGo);
+	}
+	
+	public Integer getPotentialPoints(Integer actualFloor, boolean isOpen) {
+		return getPoints(getTickToWaitToCome(actualFloor, isOpen) + tickToWait, getTickToGoToCome(actualFloor, isOpen) + tickToGo);
+	}
+	
+	public boolean willGivePoints() {
+		return getRemainingPoints() > 0;
+	}
+	
+
+	public Integer getTickToWaitToCome(Integer actualFloor, boolean isOpen) {
+		if (traveling()) {
+			return 0;
+		}
+		return getTickToCome(actualFloor, isOpen, initialFloor);
+	}
+	
+	public Integer getTickToGoToCome(Integer actualFloor, boolean isOpen) {
+		if (waiting()) {
+			return 2;
+		}
+		return getTickToCome(actualFloor, isOpen, floorToGo);
+	}
+	
+	public Integer getTickToCome(Integer actualFloor, boolean isOpen, Integer floorToCompare) {
+		if (actualFloor == floorToCompare) {
+			if (isOpen) {
+				return 0;
+			} else {
+				return 1;
+			}
+		}
+		int nbMoves = Math.abs(actualFloor - floorToCompare) + 1;
+		if (isOpen) {
+			return nbMoves + 1;
+		}
+		return nbMoves;
+	}
+	
+	public boolean willGivePointsFrom(Integer actualFloor, boolean isOpen) {
+			return getPotentialPoints(actualFloor, isOpen) > 0;
+	}
 	
 	private enum State {
 
---
+++ api/src/test/java/org/n10/codestory/s03e01/api/UserTest.java
@@ -0,0 +1,53 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.n10.codestory.s03e01.api;
+
+import org.fest.assertions.Assertions;
+import org.junit.Test;
+
+/**
+ *
+ * @author Guillaume
+ */
+public class UserTest {
+
+	@Test
+	public void testGetTickToGoToCome() {
+		User user = new User(Direction.UP, 0);
+		user.setFloorToGo(10);
+		user.travels();
+		Assertions.assertThat(user.getTickToGoToCome(0, true)).isEqualTo(10 + 1 + 1);
+	}
+	
+	@Test
+	public void testGetTickToGoToComeInTravels() {
+		User user = new User(Direction.UP, 0);
+		user.setFloorToGo(10);
+		user.travels();
+		Assertions.assertThat(user.getTickToGoToCome(4, false)).isEqualTo(6 + 1);
+	}
+	
+	@Test
+	public void testGetPotentialPointsGiveMaxAtStart() {
+		User user = new User(Direction.UP, 0);
+		user.tick();
+		user.setFloorToGo(10);
+		user.travels();
+		user.tick();
+		for (int i=0; i< 10; i++) {
+			user.tick();
+		}
+		user.tick();
+		Assertions.assertThat(user.getRemainingPoints()).isEqualTo(20);
+	}
+	
+	@Test
+	public void testPointsWhenWaiting() {
+		User user = new User(Direction.UP, 2);
+		
+		Assertions.assertThat(user.getTickToWaitToCome(0, true)).isEqualTo(4);
+		Assertions.assertThat(user.getPotentialPoints(0, true)).isEqualTo(18);
+	}
+}
